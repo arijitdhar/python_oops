255af3ebb666e3d2167c3fafc48041d99545122d
Ariit Dhar <adhar@levi.com>
2020-07-23 09:49:29 +0530

    examples of instance variable and method overriding

--- account/acc.py
+++ account/acc.py
@@ -4,11 +4,13 @@ class Account():
 
     def __init__(self, filepath):
         self.filepath=filepath
+        self.name = ">>Account"
         with open(filepath, 'r') as file:
             # balance is an instance variable, which is specific to an instance of the class
             self.balance = int(file.read())
 
     def withdraw(self, amount):
+        print("Inside parent withdraw amount")
         self.balance = self.balance - amount
 
     def deposit(self, amount):
@@ -30,23 +32,32 @@ class Checking(Account):
     def __init__(self, filepath, fee):
         Account.__init__(self, filepath)
         self.fee = fee
+        # Overriding instance variable
+        self.name = ">>Checking"
+
+    def withdraw(self, amount):
+        print("Inside overriden withdraw method")
+        Account.withdraw(self, amount)
 
     def transfer(self, amount):
         self.balance = self.balance - amount - self.fee
 
 
 jacks_checking = Checking("jack.txt", 1)
-jacks_checking.transfer(100)
+jacks_checking.withdraw(100)
 jacks_checking.commit()
 print(jacks_checking.balance)
 print(jacks_checking.type)
+print(jacks_checking.name)
 
 
 johns_checking = Checking("john.txt", 1)
 johns_checking.transfer(100)
 johns_checking.commit()
 print(johns_checking.balance)
-print(jacks_checking.type)
+print(johns_checking.type)
+print(johns_checking.name)
+
 
 
 # __doc__ generates docstring for a class
